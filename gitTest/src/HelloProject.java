bc4e2ec11ac89bf32203af4316dde28fab2a5655
cangyun <guocangyun.163.com>
2020-06-16 20:55:48 +0800

    version02

--- gitTest/src/HelloProject.java
+++ gitTest/src/HelloProject.java
@@ -1,5 +1,5 @@
 public class HelloProject {
     public static void main(String[] args) {
-        System.out.println("I love China");
+        System.out.println("I love China, I love you");
     }
 }
